7577685a875d80a4f55b8fc2eb23b295b3e3e2e3
Motsoeneg <leratok5@gmail.com>
2023-07-15 19:17:34 +0200

    added background photo

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
   return days[day];
 }
